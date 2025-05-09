2a0f7e583368f0a08fca8aad4400cde6c766a3e7
Ashwini <ashwini@phantasm.co.in>
2025-05-09 12:15:10 +0530

    Css changed for mobile view

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Contact.css";
-import { CiLinkedin } from "react-icons/ci";
+// import { CiLinkedin } from "react-icons/ci";
 
 const Contact = () => {
   const [result, setResult] = React.useState("");
